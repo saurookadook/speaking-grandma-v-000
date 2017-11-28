6daa2f3c60091ce119c884c11f96089c4c0d58c9
Andy Maskiell <maskiella@gmail.com>
2017-11-28 19:44:56 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if "#{phrase}".upcase == false
+  if phrase == "#{phrase}".upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif "#{phrase}".upcase
     return "NO, NOT SINCE 1938!"
